bc412de61949320a78f707c6598ad96ed5221fc7
rebecca allen and zachary baum <zachary@>
2016-09-28 14:11:09 -0400

    add working routes for editing and adding books/authors

--- app/app.php
+++ app/app.php
@@ -17,10 +17,52 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array ('twig.path' => __DIR__.'/../views'));
 
-    // use Symfony\Component\HttpFoundation\Request;
-    // Request::enableHttpMethodParameterOverride();
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig', array('books' => Book::getAll(), 'authors' => Author::getAll()));
+    });
+
+    $app->get("/add_book", function() use ($app) {
+        return $app['twig']->render('add-book.html.twig', array('books' => Book::getAll(), 'authors' => Author::getAll()));
+    });
+
+    $app->post("/new_book", function() use ($app) {
+        $new_book = new Book($_POST['title']);
+        $new_book->save();
+        if ($_POST['new_author'] != '')
+        {
+            $new_author = new Author($_POST['new_author']);
+            $new_author->save();
+            $new_book->addAuthor($new_author);
+        }
+        if ($_POST['author_id'] != "0")
+        {
+            $author = Author::find($_POST['author_id']);
+            $author->addBook($new_book);
+        }
+        return $app['twig']->render('home.html.twig', array('books' => Book::getAll(), 'authors' => Author::getAll()));
+    });
+
+    $app->get("/book/{id}", function($id) use ($app) {
+            return $app['twig']->render('edit-book.html.twig', array('book' => Book::find($id), 'authors' => Author::getAll()));
+    });
+
+    $app->patch("/edited_book/{id}", function($id) use ($app) {
+        $book = Book::find($id);
+        $book->update($_POST['new_title']);
+        if ($_POST['new_author'] != '')
+        {
+            $new_author = new Author($_POST['new_author']);
+            $new_author->save();
+            $book->addAuthor($new_author);
+        }
+        if ($_POST['author_id'] != "0")
+        {
+            $author = Author::find($_POST['author_id']);
+            $author->addBook($book);
+        }
         return $app['twig']->render('home.html.twig', array('books' => Book::getAll(), 'authors' => Author::getAll()));
     });
 
